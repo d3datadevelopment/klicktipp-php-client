7a216173d9d7013ac4cdec9fea7e92f92728b556
Daniel Seifert <git@daniel-seifert.com>
2024-12-22 23:38:41 +0100

    complete subscription endpoint

--- src/Entities/Subscription.php
+++ src/Entities/Subscription.php
@@ -6,8 +6,43 @@ use Doctrine\Common\Collections\ArrayCollection;
 
 class Subscription extends ArrayCollection
 {
-    public function getId(): string
+    public function getListId(): string
     {
-        return $this->get('id');
+        return $this->get('listid') ?? '';
+    }
+
+    public function getName(): string
+    {
+        return $this->get('name') ?? '';
+    }
+
+    public function getPendingUrl(): string
+    {
+        return $this->get('pendingurl') ?? '';
+    }
+
+    public function getThankyouUrl(): string
+    {
+        return $this->get('thankyouurl') ?? '';
+    }
+
+    public function useSingleOptin(): bool
+    {
+        return $this->get('usesingleoptin');
+    }
+
+    public function useDoubleOptin(): bool
+    {
+        return !$this->useSingleOptin();
+    }
+
+    public function resendConfirmationEmail(): bool
+    {
+        return $this->get('resendconfirmationemail');
+    }
+
+    public function useChangeEmail(): bool
+    {
+        return $this->get('usechangeemail');
     }
 }
